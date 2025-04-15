15950cfb731deebc1e4baf8a303da70f63b8a201
Galax <ellabisher@ufl.edu>
2025-04-14 23:19:01 -0400

    fixed naming errors

--- calculator.py
+++ calculator.py
@@ -21,21 +21,21 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Logarithm base must be > 0 and ≠ 1, and argument must be > 0.")
     return math.log(b, a)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
